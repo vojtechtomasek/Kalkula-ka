20721d6f280da5454beeca03254bbcb4627dd9d9
vojtechtomasek <113976588+vojtechtomasek@users.noreply.github.com>
2022-10-14 21:50:28 +0200

    Add files via upload

---
+++ zkouška/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Zkouska
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("lksdjflakjf");
+
+            Console.ReadKey();
+        }
+    }
+}
+
